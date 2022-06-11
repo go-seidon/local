0b07de848e5f6b03ef6fac26dbae130dd30b4981
KokoRaka <raka.suryadi@gmail.com>
2022-06-11 14:42:36 +0700

    feat: add middleware package

---
+++ internal/rest-app/middleware_test.go
@@ -0,0 +1,52 @@
+package rest_app_test
+
+import (
+	"net/http"
+
+	"github.com/go-seidon/local/internal/mock"
+	rest_app "github.com/go-seidon/local/internal/rest-app"
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = Describe("Middleware Package", func() {
+
+	Context("DefaultHeaderMiddleware", func() {
+		var (
+			r           *http.Request
+			w           *mock.MockResponseWriter
+			middleware  http.Handler
+			httpHandler *mock.MockHandler
+		)
+
+		BeforeEach(func() {
+			t := GinkgoT()
+			ctrl := gomock.NewController(t)
+
+			r = &http.Request{}
+			w = mock.NewMockResponseWriter(ctrl)
+			httpHandler = mock.NewMockHandler(ctrl)
+			middleware = rest_app.DefaultHeaderMiddleware(httpHandler)
+		})
+
+		When("middleware is called", func() {
+			It("should call serve http", func() {
+				httpHandler.EXPECT().
+					ServeHTTP(gomock.Eq(w), gomock.Eq(r)).
+					Times(1)
+
+				w.EXPECT().
+					Header().
+					Return(http.Header{}).
+					Times(1)
+
+				w.EXPECT().
+					WriteHeader(http.StatusOK).
+					Times(1)
+
+				middleware.ServeHTTP(w, r)
+			})
+		})
+	})
+
+})
