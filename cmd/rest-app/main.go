8f6f1d8d0029353b85b1efc8c907d054bf0e099d
KokoRaka <raka.suryadi@gmail.com>
2022-06-30 23:33:17 +0700

    feat: refactor rest app

--- cmd/rest-app/main.go
+++ cmd/rest-app/main.go
@@ -1,30 +1,19 @@
 package main
 
 import (
-	"github.com/go-seidon/local/internal/logging"
+	"github.com/go-seidon/local/internal/app"
 	rest_app "github.com/go-seidon/local/internal/rest-app"
 )
 
 func main() {
-	appName := "local-storage"
-	appVersion := "1.0.0"
-
-	log, err := logging.NewLogrusLog(&logging.NewLogrusLogOption{
-		AppName:    appName,
-		AppVersion: appVersion,
-	})
-	if err != nil {
-		panic(err)
-	}
-
 	app, err := rest_app.NewRestApp(&rest_app.NewRestAppOption{
 		Config: &rest_app.RestAppConfig{
-			AppName:    appName,
-			AppVersion: appVersion,
+			AppName:    "local-storage",
+			AppVersion: "1.0.0",
 			AppHost:    "localhost",
 			AppPort:    3000,
+			DbProvider: app.DB_PROVIDER_MYSQL,
 		},
-		Logger: log,
 	})
 	if err != nil {
 		panic(err)
