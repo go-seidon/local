7a2d8383fc7574a107860f7b25b1febb341a17a4
KokoRaka <raka.suryadi@gmail.com>
2022-07-02 22:51:43 +0700

    feat: update log usage

--- internal/rest-app/rest_app.go
+++ internal/rest-app/rest_app.go
@@ -26,7 +26,7 @@ type restApp struct {
 }
 
 func (a *restApp) Run() error {
-	a.logger.Info("Running %s:%s", a.config.GetAppName(), a.config.GetAppVersion())
+	a.logger.Infof("Running %s:%s", a.config.GetAppName(), a.config.GetAppVersion())
 
 	err := a.healthService.Start()
 	if err != nil {
@@ -39,7 +39,7 @@ func (a *restApp) Run() error {
 	a.server.Handler = router
 	a.server.Addr = a.config.GetAddress()
 
-	a.logger.Info("Listening in: %s", a.config.GetAddress())
+	a.logger.Infof("Listening in: %s", a.config.GetAddress())
 	err = a.server.ListenAndServe()
 	if err != http.ErrServerClosed {
 		return err
