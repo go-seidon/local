08aecc3740b311c3df7e8aaf1883c24623dd5621
KokoRaka <raka.suryadi@gmail.com>
2022-07-03 22:16:37 +0700

    feat: integrate config

--- cmd/rest-app/main.go
+++ cmd/rest-app/main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/go-seidon/local/internal/app"
+	"github.com/go-seidon/local/internal/config"
 	rest_app "github.com/go-seidon/local/internal/rest-app"
 )
 
 func main() {
+	appEnv := os.Getenv("APP_ENV")
+	if appEnv == "" {
+		appEnv = "local"
+	}
+
+	appConfig := app.Config{AppEnv: appEnv}
+
+	cfgFileName := fmt.Sprintf("config/%s.toml", appConfig.AppEnv)
+	tomlConfig, err := config.NewViperConfig(
+		config.WithFileName(cfgFileName),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	err = tomlConfig.LoadConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	err = tomlConfig.ParseConfig(&appConfig)
+	if err != nil {
+		panic(err)
+	}
+
 	app, err := rest_app.NewRestApp(
-		rest_app.WithConfig(rest_app.RestAppConfig{
-			AppName:    "local-storage",
-			AppVersion: "1.0.0",
-			AppHost:    "localhost",
-			AppPort:    3000,
-			DbProvider: app.DB_PROVIDER_MYSQL,
-		}),
+		rest_app.WithConfig(appConfig),
 	)
 	if err != nil {
 		panic(err)
