a436ffef76160b9339452f7bada78bd15e69a0e6
KokoRaka <raka.suryaardi@gmail.com>
2022-07-24 21:30:37 +0700

    feat: regenerate serializer mock

--- internal/mock/serialization_serializer_mock.go
+++ internal/mock/serialization_serializer_mock.go
@@ -33,106 +33,106 @@ func (m *MockSerializer) EXPECT() *MockSerializerMockRecorder {
 	return m.recorder
 }
 
-// Decode mocks base method.
-func (m *MockSerializer) Decode(i []byte, o interface{}) error {
+// Marshal mocks base method.
+func (m *MockSerializer) Marshal(i interface{}) ([]byte, error) {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "Decode", i, o)
-	ret0, _ := ret[0].(error)
-	return ret0
-}
-
-// Decode indicates an expected call of Decode.
-func (mr *MockSerializerMockRecorder) Decode(i, o interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Decode", reflect.TypeOf((*MockSerializer)(nil).Decode), i, o)
-}
-
-// Encode mocks base method.
-func (m *MockSerializer) Encode(i interface{}) ([]byte, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "Encode", i)
+	ret := m.ctrl.Call(m, "Marshal", i)
 	ret0, _ := ret[0].([]byte)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
 
-// Encode indicates an expected call of Encode.
-func (mr *MockSerializerMockRecorder) Encode(i interface{}) *gomock.Call {
+// Marshal indicates an expected call of Marshal.
+func (mr *MockSerializerMockRecorder) Marshal(i interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Encode", reflect.TypeOf((*MockSerializer)(nil).Encode), i)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Marshal", reflect.TypeOf((*MockSerializer)(nil).Marshal), i)
 }
 
-// MockEncoder is a mock of Encoder interface.
-type MockEncoder struct {
+// Unmarshal mocks base method.
+func (m *MockSerializer) Unmarshal(i []byte, o interface{}) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Unmarshal", i, o)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// Unmarshal indicates an expected call of Unmarshal.
+func (mr *MockSerializerMockRecorder) Unmarshal(i, o interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Unmarshal", reflect.TypeOf((*MockSerializer)(nil).Unmarshal), i, o)
+}
+
+// MockMarshaller is a mock of Marshaller interface.
+type MockMarshaller struct {
 	ctrl     *gomock.Controller
-	recorder *MockEncoderMockRecorder
+	recorder *MockMarshallerMockRecorder
 }
 
-// MockEncoderMockRecorder is the mock recorder for MockEncoder.
-type MockEncoderMockRecorder struct {
-	mock *MockEncoder
+// MockMarshallerMockRecorder is the mock recorder for MockMarshaller.
+type MockMarshallerMockRecorder struct {
+	mock *MockMarshaller
 }
 
-// NewMockEncoder creates a new mock instance.
-func NewMockEncoder(ctrl *gomock.Controller) *MockEncoder {
-	mock := &MockEncoder{ctrl: ctrl}
-	mock.recorder = &MockEncoderMockRecorder{mock}
+// NewMockMarshaller creates a new mock instance.
+func NewMockMarshaller(ctrl *gomock.Controller) *MockMarshaller {
+	mock := &MockMarshaller{ctrl: ctrl}
+	mock.recorder = &MockMarshallerMockRecorder{mock}
 	return mock
 }
 
 // EXPECT returns an object that allows the caller to indicate expected use.
-func (m *MockEncoder) EXPECT() *MockEncoderMockRecorder {
+func (m *MockMarshaller) EXPECT() *MockMarshallerMockRecorder {
 	return m.recorder
 }
 
-// Encode mocks base method.
-func (m *MockEncoder) Encode(i interface{}) ([]byte, error) {
+// Marshal mocks base method.
+func (m *MockMarshaller) Marshal(i interface{}) ([]byte, error) {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "Encode", i)
+	ret := m.ctrl.Call(m, "Marshal", i)
 	ret0, _ := ret[0].([]byte)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
 
-// Encode indicates an expected call of Encode.
-func (mr *MockEncoderMockRecorder) Encode(i interface{}) *gomock.Call {
+// Marshal indicates an expected call of Marshal.
+func (mr *MockMarshallerMockRecorder) Marshal(i interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Encode", reflect.TypeOf((*MockEncoder)(nil).Encode), i)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Marshal", reflect.TypeOf((*MockMarshaller)(nil).Marshal), i)
 }
 
-// MockDecoder is a mock of Decoder interface.
-type MockDecoder struct {
+// MockUnmarshaller is a mock of Unmarshaller interface.
+type MockUnmarshaller struct {
 	ctrl     *gomock.Controller
-	recorder *MockDecoderMockRecorder
+	recorder *MockUnmarshallerMockRecorder
 }
 
-// MockDecoderMockRecorder is the mock recorder for MockDecoder.
-type MockDecoderMockRecorder struct {
-	mock *MockDecoder
+// MockUnmarshallerMockRecorder is the mock recorder for MockUnmarshaller.
+type MockUnmarshallerMockRecorder struct {
+	mock *MockUnmarshaller
 }
 
-// NewMockDecoder creates a new mock instance.
-func NewMockDecoder(ctrl *gomock.Controller) *MockDecoder {
-	mock := &MockDecoder{ctrl: ctrl}
-	mock.recorder = &MockDecoderMockRecorder{mock}
+// NewMockUnmarshaller creates a new mock instance.
+func NewMockUnmarshaller(ctrl *gomock.Controller) *MockUnmarshaller {
+	mock := &MockUnmarshaller{ctrl: ctrl}
+	mock.recorder = &MockUnmarshallerMockRecorder{mock}
 	return mock
 }
 
 // EXPECT returns an object that allows the caller to indicate expected use.
-func (m *MockDecoder) EXPECT() *MockDecoderMockRecorder {
+func (m *MockUnmarshaller) EXPECT() *MockUnmarshallerMockRecorder {
 	return m.recorder
 }
 
-// Decode mocks base method.
-func (m *MockDecoder) Decode(i []byte, o interface{}) error {
+// Unmarshal mocks base method.
+func (m *MockUnmarshaller) Unmarshal(i []byte, o interface{}) error {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "Decode", i, o)
+	ret := m.ctrl.Call(m, "Unmarshal", i, o)
 	ret0, _ := ret[0].(error)
 	return ret0
 }
 
-// Decode indicates an expected call of Decode.
-func (mr *MockDecoderMockRecorder) Decode(i, o interface{}) *gomock.Call {
+// Unmarshal indicates an expected call of Unmarshal.
+func (mr *MockUnmarshallerMockRecorder) Unmarshal(i, o interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Decode", reflect.TypeOf((*MockDecoder)(nil).Decode), i, o)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Unmarshal", reflect.TypeOf((*MockUnmarshaller)(nil).Unmarshal), i, o)
 }
