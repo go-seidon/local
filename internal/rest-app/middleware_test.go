63c9de7dcbab8061593f9d0caccdd6b6e7560fd5
KokoRaka <raka.suryaardi@gmail.com>
2022-07-27 20:02:40 +0700

    feat: add basic auth middleware

--- internal/rest-app/middleware_test.go
+++ internal/rest-app/middleware_test.go
@@ -1,8 +1,10 @@
 package rest_app_test
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/go-seidon/local/internal/auth"
 	"github.com/go-seidon/local/internal/mock"
 	rest_app "github.com/go-seidon/local/internal/rest-app"
 	"github.com/golang/mock/gomock"
@@ -41,6 +43,152 @@ var _ = Describe("Middleware Package", func() {
 					Times(1)
 
 				middleware.ServeHTTP(w, r)
+			})
+		})
+	})
+
+	Context("NewBasicAuthMiddleware", Label("unit"), func() {
+		var (
+			a       *mock.MockBasicAuth
+			s       *mock.MockSerializer
+			handler *mock.MockHandler
+			m       http.Handler
+
+			rw  *mock.MockResponseWriter
+			req *http.Request
+		)
+
+		BeforeEach(func() {
+			t := GinkgoT()
+			ctrl := gomock.NewController(t)
+			a = mock.NewMockBasicAuth(ctrl)
+			s = mock.NewMockSerializer(ctrl)
+			handler = mock.NewMockHandler(ctrl)
+			fn := rest_app.NewBasicAuthMiddleware(a, s)
+			m = fn(handler)
+
+			rw = mock.NewMockResponseWriter(ctrl)
+			req = &http.Request{
+				Header: http.Header{},
+			}
+			req.Header.Set("Authorization", "Basic basic-token")
+		})
+
+		When("basic auth is not specified", func() {
+			It("should return error", func() {
+				req.Header.Del("Authorization")
+
+				b := rest_app.ResponseBody{
+					Code:    "UNAUTHORIZED",
+					Message: "credential is not specified",
+				}
+				s.
+					EXPECT().
+					Marshal(gomock.Eq(b)).
+					Return([]byte{}, nil).
+					Times(1)
+				rw.
+					EXPECT().
+					WriteHeader(401).
+					Times(1)
+				rw.
+					EXPECT().
+					Write(gomock.Eq([]byte{})).
+					Times(1)
+
+				m.ServeHTTP(rw, req)
+			})
+		})
+
+		When("failed check credential", func() {
+			It("should return error", func() {
+				checkParam := auth.CheckCredentialParam{
+					AuthToken: "basic-token",
+				}
+				a.
+					EXPECT().
+					CheckCredential(gomock.Any(), gomock.Eq(checkParam)).
+					Return(nil, fmt.Errorf("db error")).
+					Times(1)
+
+				b := rest_app.ResponseBody{
+					Code:    "UNAUTHORIZED",
+					Message: "failed check credential",
+				}
+				s.
+					EXPECT().
+					Marshal(gomock.Eq(b)).
+					Return([]byte{}, nil).
+					Times(1)
+				rw.
+					EXPECT().
+					WriteHeader(401).
+					Times(1)
+				rw.
+					EXPECT().
+					Write(gomock.Eq([]byte{})).
+					Times(1)
+
+				m.ServeHTTP(rw, req)
+			})
+		})
+
+		When("failed token is invalid", func() {
+			It("should return error", func() {
+				checkParam := auth.CheckCredentialParam{
+					AuthToken: "basic-token",
+				}
+				checkRes := &auth.CheckCredentialResult{
+					TokenValid: false,
+				}
+				a.
+					EXPECT().
+					CheckCredential(gomock.Any(), gomock.Eq(checkParam)).
+					Return(checkRes, nil).
+					Times(1)
+
+				b := rest_app.ResponseBody{
+					Code:    "UNAUTHORIZED",
+					Message: "credential is invalid",
+				}
+				s.
+					EXPECT().
+					Marshal(gomock.Eq(b)).
+					Return([]byte{}, nil).
+					Times(1)
+				rw.
+					EXPECT().
+					WriteHeader(401).
+					Times(1)
+				rw.
+					EXPECT().
+					Write(gomock.Eq([]byte{})).
+					Times(1)
+
+				m.ServeHTTP(rw, req)
+			})
+		})
+
+		When("token is valid", func() {
+			It("should return result", func() {
+				checkParam := auth.CheckCredentialParam{
+					AuthToken: "basic-token",
+				}
+				checkRes := &auth.CheckCredentialResult{
+					TokenValid: true,
+				}
+				a.
+					EXPECT().
+					CheckCredential(gomock.Any(), gomock.Eq(checkParam)).
+					Return(checkRes, nil).
+					Times(1)
+
+				handler.
+					EXPECT().
+					ServeHTTP(gomock.Eq(rw), gomock.Eq(req)).
+					Times(1)
+
+				m.ServeHTTP(rw, req)
 			})
 		})
 	})
